d50553cf5ef14710d075f0be170846f446dcc56d
Kimiko <kimizqh@foxmail.com>
2016-05-10 20:45:53 +0800

    first cimmit

---
+++ FreeTime/HallsViewController.h
@@ -0,0 +1,13 @@
+//
+//  HallsViewController.h
+//  FreeTime
+//
+//  Created by 周祺华 on 16/5/5.
+//  Copyright © 2016年 周祺华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HallsViewController : UIViewController
+
+@end
---
+++ FreeTime/HallsViewController.m
@@ -0,0 +1,88 @@
+//
+//  HallsViewController.m
+//  FreeTime
+//
+//  Created by 周祺华 on 16/5/5.
+//  Copyright © 2016年 周祺华. All rights reserved.
+//
+
+#import "HallsViewController.h"
+
+@interface HallsViewController ()
+
+@end
+
+@implementation HallsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return @"影院信息";
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    long row = indexPath.row;
+    long section = indexPath.section;
+    
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"tmpCell" forIndexPath:indexPath];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 60;
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayHeaderView:(nonnull UIView *)view forSection:(NSInteger)section
+{
+    view.tintColor = [UIColor whiteColor];
+    UITableViewHeaderFooterView *header = (UITableViewHeaderFooterView *)view;
+    header.textLabel.textColor = [UIColor grayColor];
+    header.textLabel.font = [UIFont systemFontOfSize:14];
+}
+
+
+@end
