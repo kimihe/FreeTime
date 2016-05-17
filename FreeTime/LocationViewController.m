54167587731d52d957dc633995b850cf23cdbc8c
Kimiko <kimizqh@foxmail.com>
2016-05-17 15:09:27 +0800

    add latitude-longitude

---
+++ FreeTime/LocationViewController.m
@@ -0,0 +1,42 @@
+//
+//  LocationViewController.m
+//  FreeTime
+//
+//  Created by 周祺华 on 16/5/17.
+//  Copyright © 2016年 周祺华. All rights reserved.
+//
+
+#import "LocationViewController.h"
+
+@interface LocationViewController ()
+
+@end
+
+@implementation LocationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)leftBarButtonItemAction:(UIBarButtonItem *)sender
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+@end
